861ea15b17555c9fff12aa3cbc52d8094a85140f
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-11-16 20:11:08 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeFolhaPagamento_POO/Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeFolhaPagamento_POO
+namespace Course.Entities
+{
+    class Employee
+    {
+        public string Nome { get; set; }
+        public int Horas { get; set; }
+        public double ValorPorHora { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string nome, int horas, double valorPorHora)
+        {
+            Nome = nome;
+            Hora = hora;
+            ValorPorHora = valorPorHora;
+        }
+
+        public virtual double Pagamento()
+        {
+            return Hora * ValorPorHora;
+        }
+    }
+}
